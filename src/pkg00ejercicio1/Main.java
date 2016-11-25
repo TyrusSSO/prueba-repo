5744f9faa2ad2feb193cd7ea9c229abffd2fdaa9
Sergi Sanchez Orts <sergisanchez23@gmail.com>
2016-11-25 16:10:09 +0100

    alta del proyecto

---
+++ src/pkg00ejercicio1/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg00ejercicio1;
+
+/**
+ *
+ * @author Tyrus
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int a2=3,b2=2;
+       System.out.println("La suma de: "+a2+"+"+b2+" es: "+a2+b2);
+       System.out.println("La suma de: "+a2+"+"+b2+" es: "+(a2+b2));
+       
+       int a = 2,b = 10;
+    }
+    
+}
